42a01850597410790feb0a396e23c6e247af7871
imcookiedabadee <ohitscookiemfl@gmail.com>
2024-02-24 14:38:40 -0500

    player movement done i think

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,22 +10,20 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpSpeed;
-    [SerializeField] private float baseGravityMultipler;
+    [SerializeField] private float baseGravityMultiplier;
     [SerializeField] private float fallGravityMultiplier;
     [SerializeField] private float coyoteTime;
     private float coyTime;
 
     Vector2 input;
     Vector2 velocity;
-    private float fallSpeed;
     bool reachedJumpPeak;
     [SerializeField] private float maxFallSpeed;
 
     [Header("Grounded")]
     private bool isGrounded;
     [SerializeField] private LayerMask whatIsGround;
-    [SerializeField] private Vector3 groundCheckBoxSize;
-    [SerializeField] private Transform groundCheckCenter;
+    [SerializeField] private float groundCheckDist;
 
     // Start is called before the first frame update
     void Start()
@@ -42,39 +40,57 @@ public class PlayerController : MonoBehaviour
 
     void Movement()
     {
-        isGrounded = Physics.OverlapBox(groundCheckCenter.position, groundCheckBoxSize * .5f, Quaternion.identity, whatIsGround).Length > 0 && rb.velocity.y <= 0;
+        //float groundCheckDistScaled = groundCheckDist * Mathf.Abs(rb.velocity.y < 0 ? rb.velocity.y * .25f : 0) / maxFallSpeed;
+        Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, groundCheckDist + transform.localScale.y, whatIsGround);
+        isGrounded = (hit.collider != null && hit.collider.tag == "Ground");
 
-        if (!isGrounded && coyTime > 0f) coyTime -= Time.deltaTime;
-
-        if (Input.GetKeyDown(KeyCode.Space) && coyTime >= 0f)
+        if(Input.GetKeyDown(KeyCode.Space) && coyTime > 0f)
         {
             velocity.y = jumpSpeed;
-            fallSpeed = 0f;
+            coyTime = 0f;
         }
-        else if (!reachedJumpPeak && Input.GetKeyUp(KeyCode.Space))
+        else if (Input.GetKeyUp(KeyCode.Space) && !reachedJumpPeak)
+        {
+            velocity.y *= .35f;
+            reachedJumpPeak = true;
+        }
+        else if (rb.velocity.y <= 0f && !isGrounded)
         {
             reachedJumpPeak = true;
-            velocity.y *= .25f;
-        }
-        else if (velocity.y <= 0f)
-        {
-            reachedJumpPeak = true;
-            fallSpeed += Physics.gravity.y * (fallGravityMultiplier - 1f) * Time.deltaTime;
+            velocity.y += Physics.gravity.y * (fallGravityMultiplier - baseGravityMultiplier) * Time.deltaTime;
         }
 
-        else fallSpeed = Mathf.Clamp(fallSpeed + baseGravityMultipler * Physics.gravity.y * Time.deltaTime, -maxFallSpeed, 0f);
-
-        if (isGrounded && velocity.y <= 0f)
+        if(isGrounded && coyTime <= 0f)
         {
-            fallSpeed = 0f;
-            reachedJumpPeak = false;
-            velocity.y = 0f;
             coyTime = coyoteTime;
         }
+        else if (coyTime > 0f)
+        {
+            coyTime -= Time.deltaTime;
+        }
+
+        if (isGrounded && velocity.y < 0f)
+        {
+            reachedJumpPeak = false;
+            velocity.y = 0;
+        }
+        else
+        {
+            velocity.y += Physics.gravity.y * baseGravityMultiplier * Time.deltaTime;
+        }
+
+        velocity.y = Mathf.Clamp(velocity.y, -maxFallSpeed, jumpSpeed);
 
         velocity.x = Mathf.SmoothDamp(velocity.x, input.x * moveSpeed, ref currXVel, xVelSmoothTime);
-        velocity.y += fallSpeed;
-
         rb.velocity = velocity;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (rb)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + Vector3.down * (groundCheckDist * rb.velocity.y / maxFallSpeed + transform.localScale.y));
+        }
     }
 }
