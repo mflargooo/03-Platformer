eace284f5fd80f9e20cc7a37bfa17d8a36356412
imcookiedabadee <ohitscookiemfl@gmail.com>
2024-02-25 22:00:42 -0500

    idk whats wrong

--- Assets/Scripts/GenerateTrack.cs
+++ Assets/Scripts/GenerateTrack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GenerateTrack : MonoBehaviour
 {
-    public static List<Cell> randomPath (Cell[,] grid, int si, int sj, int maxTracks)
+    public static List<Cell> randomPath(Cell[,] grid, int si, int sj, int maxTracks)
     {
         int dir = -1;
         int gridSize = Mathf.RoundToInt(Mathf.Sqrt(grid.Length));
@@ -16,19 +16,19 @@ public class GenerateTrack : MonoBehaviour
         while (sj < gridSize - 1 && maxTracks > 0)
         {
             dir = Random.Range(0, 3);
-            if(dir == 0 && si > 1 /* left */)
+            if (dir == 0 && si > 1 /* left */)
             {
                 if (!grid[si - 1, sj].searched)
                     si--;
             }
-            else if(dir == 1 /* up */)
+            else if (dir == 1 /* up */)
             {
-                if(!grid[si, sj + 1].searched)
+                if (!grid[si, sj + 1].searched)
                     sj++;
             }
-            else if(/* dir == 2  && right */ si < gridSize - 1)
+            else if (/* dir == 2  && right */ si < gridSize - 1)
             {
-                if(!grid[si + 1, sj].searched)
+                if (!grid[si + 1, sj].searched)
                     si++;
             }
 
@@ -52,4 +52,5 @@ public class GenerateTrack : MonoBehaviour
 
         return path;
     }
+
 }
