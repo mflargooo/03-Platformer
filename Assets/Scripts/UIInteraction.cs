cc99ca0503594f8f7733722e6aa5ebf405616cda
Apple <81790850+Sunzhuofan-Apple@users.noreply.github.com>
2024-03-02 07:51:00 -0500

    karaoke selection

---
+++ Assets/Scripts/UIInteraction.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIInteraction : MonoBehaviour, IPointerEnterHandler 
+{
+    public AudioSource audioSource;
+    public string MusicDifficulty = null;
+
+    void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (audioSource != null)
+        {
+            AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+            foreach (var source in allAudioSources)
+            {
+                if (source != audioSource) {source.Stop();}
+            }
+            audioSource.Play();
+        }
+    }
+    public void OnPointerClickEasy(PointerEventData eventData)
+    {
+        SetMusicDifficulty("easy");
+    }
+
+    public void OnPointerClickMedium(PointerEventData eventData)
+    {
+        SetMusicDifficulty("Medium");
+    }
+    public void OnPointerClickHard(PointerEventData eventData)
+    {
+        SetMusicDifficulty("Hard");
+    }
+
+    public void SetMusicDifficulty(string difficulty)
+    {
+        MusicDifficulty = difficulty;
+    }
+}
+
