f48c139552a75a1f082c33d975c87b58a50e705a
Apple <81790850+Sunzhuofan-Apple@users.noreply.github.com>
2024-03-04 23:43:04 -0500

    cafe

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Interaction : MonoBehaviour
+public class InteractionManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
